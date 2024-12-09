0d7818490a8f4962f7aacf26cbbbc88c4709a03d
mohamedgaber-136 <mohamedd.gaberr.13696@gmail.com>
2024-12-09 18:01:02 +0200

    update btn background

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 import Spline from '@splinetool/react-spline/next';
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function mainPage() {
   return (
+    <>
+        <Head>
+        <link rel="preload" href="https://prod.spline.design/2LBMtCA56PeK6vu0/scene.splinecode" as="fetch" crossOrigin="anonymous" />
+      </Head>
     <main>
       <Spline
         scene="https://prod.spline.design/2LBMtCA56PeK6vu0/scene.splinecode" 
@@ -12,5 +17,6 @@ export default function mainPage() {
       </button>
         <Link href='/home' className='btnTestTwo'></Link>
     </main>
+    </>
   );
 }
